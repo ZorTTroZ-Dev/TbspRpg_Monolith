6fefb9687c95b03d841314acdd37883cbd0450b4
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-08-04 22:08:37 -0400

    TbspRpgApi
    Added method to retrieve specific content for a game.

--- TbspRpgDataLayer.Tests/Repositories/ContentsRepositoryTests.cs
+++ TbspRpgDataLayer.Tests/Repositories/ContentsRepositoryTests.cs
@@ -42,5 +42,76 @@ namespace TbspRpgDataLayer.Tests.Repositories
         }
 
         #endregion
+
+        #region GetContentForGameAtPosition
+
+        [Fact]
+        public async void GetContentForGameAtPosition_Exists_ReturnContent()
+        {
+            // arrange
+            await using var context = new DatabaseContext(DbContextOptions);
+            var testContent = new Content()
+            {
+                Id = Guid.NewGuid(),
+                Position = 42,
+                GameId = Guid.NewGuid()
+            };
+            context.Contents.Add(testContent);
+            await context.SaveChangesAsync();
+            var repository = CreateRepository(context);
+            
+            // act
+            var content = await repository.GetContentForGameAtPosition(testContent.GameId, 42);
+            
+            // assert
+            Assert.NotNull(content);
+            Assert.Equal(testContent.Id, content.Id);
+        }
+        
+        [Fact]
+        public async void GetContentForGameAtPosition_InvalidGame_ReturnNull()
+        {
+            // arrange
+            await using var context = new DatabaseContext(DbContextOptions);
+            var testContent = new Content()
+            {
+                Id = Guid.NewGuid(),
+                Position = 42,
+                GameId = Guid.NewGuid()
+            };
+            context.Contents.Add(testContent);
+            await context.SaveChangesAsync();
+            var repository = CreateRepository(context);
+            
+            // act
+            var content = await repository.GetContentForGameAtPosition(Guid.NewGuid(), 42);
+            
+            // assert
+            Assert.Null(content);
+        }
+        
+        [Fact]
+        public async void GetContentForGameAtPosition_InvalidPosition_ReturnNull()
+        {
+            // arrange
+            await using var context = new DatabaseContext(DbContextOptions);
+            var testContent = new Content()
+            {
+                Id = Guid.NewGuid(),
+                Position = 42,
+                GameId = Guid.NewGuid()
+            };
+            context.Contents.Add(testContent);
+            await context.SaveChangesAsync();
+            var repository = CreateRepository(context);
+            
+            // act
+            var content = await repository.GetContentForGameAtPosition(testContent.GameId, 40);
+            
+            // assert
+            Assert.Null(content);
+        }
+
+        #endregion
     }
 }
--- TbspRpgDataLayer/Repositories/ContentsRepository.cs
+++ TbspRpgDataLayer/Repositories/ContentsRepository.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using TbspRpgApi.Entities;
 
 namespace TbspRpgDataLayer.Repositories
@@ -5,6 +9,7 @@ namespace TbspRpgDataLayer.Repositories
     public interface IContentsRepository : IBaseRepository
     {
         void AddContent(Content content);
+        Task<Content> GetContentForGameAtPosition(Guid gameId, ulong position);
     }
     
     public class ContentsRepository : IContentsRepository
@@ -20,7 +25,13 @@ namespace TbspRpgDataLayer.Repositories
         {
             await _databaseContext.AddAsync(content);
         }
-        
+
+        public Task<Content> GetContentForGameAtPosition(Guid gameId, ulong position)
+        {
+            return _databaseContext.Contents.AsQueryable()
+                .FirstOrDefaultAsync(content => content.GameId == gameId && content.Position == position);
+        }
+
         public async void SaveChanges()
         {
             await _databaseContext.SaveChangesAsync();
