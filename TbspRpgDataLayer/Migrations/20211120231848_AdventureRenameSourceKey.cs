042ed7e430da74375be3e38eddcfcb7b88e8d9f5
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-11-20 18:19:54 -0500

    UI Admin Interface
    Updated database from property rename.

---
+++ TbspRpgDataLayer/Migrations/20211120231848_AdventureRenameSourceKey.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TbspRpgDataLayer.Migrations
+{
+    public partial class AdventureRenameSourceKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SourceKey",
+                table: "adventures",
+                newName: "InitialSourceKey");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "InitialSourceKey",
+                table: "adventures",
+                newName: "SourceKey");
+        }
+    }
+}
