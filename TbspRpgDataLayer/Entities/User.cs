9c33c88ca3f7eaed7c53353e04caf9259d561581
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-12-18 14:25:25 -0500

    Initial Release
    Made user model changes required for registration.

--- TbspRpgDataLayer/Entities/User.cs
+++ TbspRpgDataLayer/Entities/User.cs
@@ -8,6 +8,10 @@ namespace TbspRpgDataLayer.Entities
         public Guid Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string RegistrationKey { get; set; }
+        public bool RegistrationComplete { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? LastLogin { get; set; }
         
         public ICollection<Game> Games { get; set; }
         public ICollection<Adventure> Adventures { get; set; } 
---
+++ TbspRpgDataLayer/Migrations/20211218181901_UpdateUserForRegistration.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TbspRpgDataLayer.Migrations
+{
+    public partial class UpdateUserForRegistration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "user",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLogin",
+                table: "user",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RegistrationComplete",
+                table: "user",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RegistrationKey",
+                table: "user",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "user");
+
+            migrationBuilder.DropColumn(
+                name: "LastLogin",
+                table: "user");
+
+            migrationBuilder.DropColumn(
+                name: "RegistrationComplete",
+                table: "user");
+
+            migrationBuilder.DropColumn(
+                name: "RegistrationKey",
+                table: "user");
+        }
+    }
+}
