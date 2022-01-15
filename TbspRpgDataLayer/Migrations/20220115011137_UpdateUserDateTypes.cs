2c6bf26ec766e3c16cd67730f7f29ebe7fda7b3c
cdvanhorn <cdavid.vanhorn@gmail.com>
2022-01-14 21:20:04 -0500

    Initial Release
    * Updated date time fields on user
    * Added migration to seed user accounts and groups

---
+++ TbspRpgDataLayer/Migrations/20220115011137_UpdateUserDateTypes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TbspRpgDataLayer.Migrations
+{
+    public partial class UpdateUserDateTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastLogin",
+                table: "user",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateCreated",
+                table: "user",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastLogin",
+                table: "user",
+                type: "timestamp without time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateCreated",
+                table: "user",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
--- migration_env.ps1
+++ migration_env.ps1
@@ -1,1 +1,4 @@
 $Env:CONNECTION_STRING="User ID=postgres;Password=tbsprpg;Server=localhost;Port=5432;Database=tbsprpgapi;Integrated Security=true;Pooling=true;"
+$Env:DATABASE_SALT="7LaxiKnQXhoiTAiQeJyx2w=="
+$Env:ADMIN_PASSWORD="admin"
+$Env:TEST_PASSWORD="test"
