0bbbd983d321311b9520eef125a1221e97cf4736
cdvanhorn <34102449+cdvanhorn@users.noreply.github.com>
2022-04-26 19:23:19 -0400

    Lua Scripts
    Add a return type to the script object.

--- TbspRpgDataLayer/Entities/Script.cs
+++ TbspRpgDataLayer/Entities/Script.cs
@@ -9,6 +9,7 @@ public class Script
     public string Name { get; set; }
     public string Type { get; set; }
     public string Content { get; set; }
+    public string ReturnType { get; set; }
     
     public ICollection<Script> IncludedIn { get; set; }
     public ICollection<Script> Includes { get; set; }
