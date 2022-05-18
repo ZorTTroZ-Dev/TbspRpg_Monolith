d83757cd80ed22f5b6f3451ab17286124b60db39
cdvanhorn <34102449+cdvanhorn@users.noreply.github.com>
2022-05-17 21:25:42 -0400

    Lua Scripts
    Implemented API tests for retrieving scripts for an adventure.

--- TbspRpgApi.Tests/Controllers/ScriptsControllerTests.cs
+++ TbspRpgApi.Tests/Controllers/ScriptsControllerTests.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging.Abstractions;
 using TbspRpgApi.Controllers;
+using TbspRpgApi.ViewModels;
 using TbspRpgDataLayer.Entities;
 using Xunit;
 
@@ -21,13 +24,65 @@ public class ScriptsControllerTests: ApiTest
     [Fact]
     public async void GetsScriptsForAdventure_HasScripts_ReturnList()
     {
+        // arrange
+        var testScripts = new List<Script>()
+        {
+            new Script()
+            {
+                Id = Guid.NewGuid(),
+                AdventureId = Guid.NewGuid(),
+                Name = "test"
+            },
+            new Script()
+            {
+                Id = Guid.NewGuid(),
+                AdventureId = Guid.NewGuid(),
+                Name = "test two"
+            }
+        };
+        var controller = CreateController(testScripts);
         
+        // act
+        var response = await controller.GetScriptsForAdventure(testScripts[0].AdventureId);
+        
+        // assert
+        var okObjectResult = response as OkObjectResult;
+        Assert.NotNull(okObjectResult);
+        var scriptViewModels = okObjectResult.Value as List<ScriptViewModel>;
+        Assert.NotNull(scriptViewModels);
+        Assert.Single(scriptViewModels);
     }
     
     [Fact]
     public async void GetsScriptsForAdventure_NoScripts_ReturnEmpty()
     {
+        // arrange
+        var testScripts = new List<Script>()
+        {
+            new Script()
+            {
+                Id = Guid.NewGuid(),
+                AdventureId = Guid.NewGuid(),
+                Name = "test"
+            },
+            new Script()
+            {
+                Id = Guid.NewGuid(),
+                AdventureId = Guid.NewGuid(),
+                Name = "test two"
+            }
+        };
+        var controller = CreateController(testScripts);
         
+        // act
+        var response = await controller.GetScriptsForAdventure(Guid.NewGuid());
+        
+        // assert
+        var okObjectResult = response as OkObjectResult;
+        Assert.NotNull(okObjectResult);
+        var scriptViewModels = okObjectResult.Value as List<ScriptViewModel>;
+        Assert.NotNull(scriptViewModels);
+        Assert.Empty(scriptViewModels);
     }
 
     #endregion
--- TbspRpgApi.Tests/Services/ScriptsServiceTests.cs
+++ TbspRpgApi.Tests/Services/ScriptsServiceTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using TbspRpgDataLayer.Entities;
 using Xunit;
 
 namespace TbspRpgApi.Tests.Services;
@@ -9,13 +12,58 @@ public class ScriptsServiceTests: ApiTest
     [Fact]
     public async void GetsScriptsForAdventure_HasScripts_ReturnList()
     {
+        // arrange
+        var testScripts = new List<Script>()
+        {
+            new Script()
+            {
+                Id = Guid.NewGuid(),
+                AdventureId = Guid.NewGuid(),
+                Name = "test"
+            },
+            new Script()
+            {
+                Id = Guid.NewGuid(),
+                AdventureId = Guid.NewGuid(),
+                Name = "test two"
+            }
+        };
+        var service = CreateScriptsService(testScripts);
         
+        // act
+        var scripts = await service.GetScriptsForAdventure(testScripts[0].AdventureId);
+        
+        // assert
+        Assert.Single(scripts);
+        Assert.Equal("test", scripts[0].Name);
     }
     
     [Fact]
     public async void GetsScriptsForAdventure_NoScripts_ReturnEmpty()
     {
+        // arrange
+        var testScripts = new List<Script>()
+        {
+            new Script()
+            {
+                Id = Guid.NewGuid(),
+                AdventureId = Guid.NewGuid(),
+                Name = "test"
+            },
+            new Script()
+            {
+                Id = Guid.NewGuid(),
+                AdventureId = Guid.NewGuid(),
+                Name = "test two"
+            }
+        };
+        var service = CreateScriptsService(testScripts);
         
+        // act
+        var scripts = await service.GetScriptsForAdventure(Guid.NewGuid());
+        
+        // assert
+        Assert.Empty(scripts);
     }
 
     #endregion
