6fb9a45f75329e3e3dfc94d8b48c096ec94b9264
cdvanhorn <cdavid.vanhorn@gmail.com>
2022-11-12 19:51:31 -0500

    feature/TRAPI-40 Adventure Object Entity
    Working on adding data layer repository and service layer code.

---
+++ TbspRpgDataLayer/Repositories/AdventureObjectRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TbspRpgDataLayer.Entities;
+
+namespace TbspRpgDataLayer.Repositories;
+
+public interface IAdventureObjectRepository: IBaseRepository
+{
+    Task<AdventureObject> GetAdventureObjectById(Guid adventureObjectId);
+    Task<List<AdventureObject>> GetAdventureObjectsForAdventure(Guid adventureId);
+    Task AddAdventureObject(AdventureObject adventureObject);
+    void RemoveAdventureObject(AdventureObject adventureObject);
+    void RemoveAdventureObjects(ICollection<AdventureObject> adventureObjects);
+}
+
+public class AdventureObjectRepository: IAdventureObjectRepository
+{
+    private readonly DatabaseContext _databaseContext;
+    
+    public AdventureObjectRepository(DatabaseContext databaseContext)
+    {
+        _databaseContext = databaseContext;
+    }
+    
+    public async Task SaveChanges()
+    {
+        await _databaseContext.SaveChangesAsync();
+    }
+
+    public async Task<AdventureObject> GetAdventureObjectById(Guid adventureObjectId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<AdventureObject>> GetAdventureObjectsForAdventure(Guid adventureId)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public async Task AddAdventureObject(AdventureObject adventureObject)
+    {
+        await _databaseContext.AdventureObjects.AddAsync(adventureObject);
+    }
+    
+    public void RemoveAdventureObject(AdventureObject adventureObject)
+    {
+        _databaseContext.Remove(adventureObject);
+    }
+
+    public void RemoveAdventureObjects(ICollection<AdventureObject> adventureObjects)
+    {
+        _databaseContext.RemoveRange(adventureObjects);
+    }
+}
---
+++ TbspRpgDataLayer/Services/AdventureObjectService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using TbspRpgDataLayer.Entities;
+using TbspRpgDataLayer.Repositories;
+
+namespace TbspRpgDataLayer.Services;
+
+public interface IAdventureObjectService : IBaseService
+{
+    Task<AdventureObject> GetAdventureObjectById(Guid adventureObjectId);
+    Task<List<AdventureObject>> GetAdventureObjectsForAdventure(Guid adventureId);
+    Task AddAdventureObject(AdventureObject adventureObject);
+    void RemoveAdventureObject(AdventureObject adventureObject);
+    void RemoveAdventureObjects(ICollection<AdventureObject> adventureObjects);
+}
+
+public class AdventureObjectService: IAdventureObjectService
+{
+    private readonly IAdventureObjectRepository _adventureObjectRepository;
+    private readonly ILogger<AdventureObjectService> _logger;
+    
+    public AdventureObjectService(IAdventureObjectRepository adventureObjectRepository,
+        ILogger<AdventureObjectService> logger)
+    {
+        _adventureObjectRepository = adventureObjectRepository;
+        _logger = logger;
+    }
+    
+    public async Task SaveChanges()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public async Task<AdventureObject> GetAdventureObjectById(Guid adventureObjectId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<AdventureObject>> GetAdventureObjectsForAdventure(Guid adventureId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task AddAdventureObject(AdventureObject adventureObject)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveAdventureObject(AdventureObject adventureObject)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveAdventureObjects(ICollection<AdventureObject> adventureObjects)
+    {
+        throw new NotImplementedException();
+    }
+}
